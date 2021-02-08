a0c8e219b1ec24fa348a6baf9b219a635c3168e3
hello23organic <hello23organic@gmail.com>
2021-02-07 16:32:52 -0800

    test new file

--- lab2setup/GetEnvironmentVariables.java
+++ lab2setup/GetEnvironmentVariables.java
@@ -1,7 +1,7 @@
 public class GetEnvironmentVariables {
 
     //  Fill in the path to your fa20-s*** folder between the quotes
-    public static String REPO_DIR = "";
+    public static String REPO_DIR = "/Users/junjun/Desktop/cs61b";
 
     //  Fill in the path to your snaps-fa20-s*** folder between the quotes
     public static String SNAPS_DIR = "";
